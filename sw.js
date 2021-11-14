a0bfb19fffd0d29a94a125d3ffd3d4d50d43cbd9
robo-mop <realaryanpingle@gmail.com>
2021-11-15 03:43:21 +0530

    SW shenanigans offline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 11
+const CACHE_VERSION = 12
 const CURRENT_CACHE = `cache-${CACHE_VERSION}`
 
 const cache_files = ["/index.html", "/main.js", "/main.css", "/images/", "/offline/"]
