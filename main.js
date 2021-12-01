363e79f3d9f61fdf615707aeedf4973567eff9d6
robo-mop <realaryanpingle@gmail.com>
2021-12-01 16:32:47 +0530

    1-12-21

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ function setup() {
     }
     print(items)
     for (let [category, list] of Object.entries(items)) {
-        list = list[0].split(", ")
         document.getElementById(category).lastElementChild.innerHTML = list.map(ele => `<div class="item">${ele}</div>`).join("")
     }
 }
@@ -44,45 +43,60 @@ window.onload = () => {
 
 var menu = {
     "Monday": {
-        "B": ["Masala Poha, Tea + Coffee, Bread Jam"],
-        "L": ["Bhendi, Sprout Matki, Dal Tadka, Rice, Pickle, Chapati, Sweet Lassi, Papad"],
-        "S": ["Vada Pav, Tea + Coffee"],
-        "D": ["Aloo Gobi, Black Chana, Rice + Dal, Chapati, Pickle, Papad"]
+        "B": "Onion Poha",
+        "L": "Shahi Bhendi, Sprout Matki, Dal Tadka, Rice, Pickle, Chapati, Sweet Lassi, Papad",
+        "S": "Vada Pav",
+        "D": "Aloo Gobi, Black Chana, Dal, Jeera Rice, Chapati, Pickle, Papad"
     },
     "Tuesday": {
-        "B": ["Idli Sambar, Tea + Coffee, Bread Jam"],
-        "L": ["Punjabi Chole, Aloo Jeera, Lasuni Dal, Rice, Pickle, Puri, Lime Juice, Mix Salad"],
-        "S": ["Bhelpuri, Tea + Coffee"],
-        "D": ["Mix Veg Dry, Chawali Masala, Dal Khichdi, Chapati, Pickle, Mong Salad"]
+        "B": "Idli Sambar",
+        "L": "Punjabi Chole, Aloo Jeera, Lasuni Dal, Rice, Pickle, Puri, Lime Juice, Mix Salad",
+        "S": "Bhelpuri",
+        "D": "Veg Diwani Handi, Chawali Masala, Dal Kholapuri, Chapati, Pickle, Mong Salad"
     },
     "Wednesday": {
-        "B": ["Aloo Paratha, Tea + Coffee, Bread Jam"],
-        "L": ["Dum Aloo Gravy, Cabbage, Dal Fry, Rice, Chapati, Pickle, Butter Milk, Papad"],
-        "S": ["Masala Sandwich, Tea + Coffee"],
-        "D": ["Paneer Gravy, Chicken Gravy, Lauki / Kaddu Masala, Dal Lasuni, Rice, Chapati, Pickle, Papad"]
+        "B": "Aloo Paratha",
+        "L": "Methi Malai Matar, Cabbage, Dal Makhani, Rice, Chapati, Rasam, Pickle, Butter Milk, Papad",
+        "S": "Masala Sandwich",
+        "D": "Paneer Biryani, Chicken Biryani, Lauki / Kaddu Masala, Dal Lasuni, Rice, Chapati, Pickle, Raita"
     },
     "Thursday": {
-        "B": ["Upma / Pav Bhaji, Tea + Coffee, Bread Jam"],
-        "L": ["Soya Masala Gravy, Beans Fugat (Dry), Dal Makhani, Rice, Pickle, Chapati, Tang, Fryums"],
-        "S": ["Veg Maggi, Tea + Coffee"],
-        "D": ["Mix Sprout Gravy, Aloo Palak, Dal Adraki, Veg Pulao, Chapati, Pickle, Green Salad"]
+        "B": "Upma / Pav Bhaji",
+        "L": "Soya Masala Gravy, Beans Fugat (Dry), Dal Masala, Rice, Pickle, Chapati, Curd, Fryums",
+        "S": "Veg Maggi",
+        "D": "Aloo Palak, Veg Jalfrizi, Dal Adraki, Veg Pulao, Chapati, Pickle, Green Salad"
     },
     "Friday": {
-        "B": ["Uttappa / Medhu Wada, Tea + Coffee, Bread Jam"],
-        "L": ["Veg Diwani Handi, Rajma Masala, Dal Fry, Rice, Pickle, Chapati, Dahi, Salad"],
-        "S": ["Samosa, Tea + Coffee"],
-        "D": ["White Watana Gravy, Bhendi Masala, Dal Tadka, Rice, Pickle, Chapati, Papad"]
+        "B": "Uttappa / Medhu Wada",
+        "L": "Veg Diwani Handi, Rajma Masala, Kaali Masoori Dal, Rice, Pickle, Chapati, Rasam, Tang, Salad",
+        "S": "Samosa",
+        "D": "Chicken / Egg Masala, Bhendi Masala, Dal Tadka, Rice, Pickle, Chapati, Papad"
     },
     "Saturday": {
-        "B": ["Masala Dosa, Tea + Coffee, Bread Jam"],
-        "L": ["Aloo Capsicum, Kadi Pakoda, Mix Dal, Rice, Pickle, Chapati, Tang, Papad"],
-        "S": ["Veg Sandwich, Tea + Coffee"],
-        "D": ["Dry Soya, Moong Usal, Rassam, Lemon Rice, Pickle, Chapati, Green Salad"]
+        "B": "Masala Dosa",
+        "L": "Aloo Capsicum, Kadi Pakoda, Mix Dal, Rice, Pickle, Chapati, Papad",
+        "S": "Dabeli",
+        "D": "Dry Soya, Moong Usal, Rassam, Lemon Rice, Pickle, Chapati, Green Salad"
     },
     "Sunday": {
-        "B": ["Chole Bhature, Tea + Coffee, Bread Jam"],
-        "L": ["Veg Kuram GRavy, Veg Biryani, Dal Fry, Rice, Pickle, Chapati, Veg Raita, Green Salad"],
-        "S": ["Bread Pakoda, Tea + Coffee"],
-        "D": ["Veg Manchurian, Green Peas Gravy, Dal, Veg Fried Rice, Pickle, Chapati, Papad"]
+        "B": "Chole Bhature",
+        "L": "Veg Kuram Gravy, Veg Biryani, Dal Fry, Rice, Pickle, Chapati, Veg Raita, Green Salad",
+        "S": "Bread Pakoda",
+        "D": "Veg Manchurian, Green Peas Gravy, Dal, Veg Fried Rice, Pickle, Chapati, Papad"
+    }
+}
+
+var everyday = {
+    "B": "Tea + Coffee, Boiled Egg / Omelette, Bread + Jam, Hot + Cold Milk",
+    "L": "",
+    "S": "Tea + Coffee",
+    "D": ""
+}
+
+for(let [day, list] of Object.entries(menu)) {
+    for(let [category, items] of Object.entries(menu[day])) {
+        items = items.split(", ")
+        if(everyday[category] != '') items.push(...everyday[category].split(", "))
+        menu[day][category] = items
     }
 }
